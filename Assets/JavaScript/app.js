73ba3d7020b718df335979e1f5960582358b18f4
Patrick ONeill <poneill@sembracare.com>
2019-09-23 20:07:08 -0400

    adding OMDB API call

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -14,25 +14,39 @@ $(document).ready(function () {
       }
     }
 
-    $.ajax(settings).then(function (response) {
-      console.log(response);
-    });
+    $.ajax(settings).then(function (netflixResponse) {
+      console.log(netflixResponse);
 
-    //New Releases Query
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv",
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
-        "x-rapidapi-key": "e138a5e326mshee193d9b02e3dafp19cc7djsn4aa0ac2244fa"
+      for (let i = 0; i < 3; i++) {
+        var queryURL = "https://www.omdbapi.com/?i=" + netflixResponse.ITEMS[i].imdbid + "&apikey=trilogy";
+
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function (omdbResponse) {
+          console.log(omdbResponse);
+        });
       }
-    }
-
-    $.ajax(settings).done(function (response) {
-      console.log(response);
     });
+
+
+
+    // //New Releases Query
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7%3AUS&p=1&t=ns&st=adv",
+    //   "method": "GET",
+    //   "headers": {
+    //     "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+    //     "x-rapidapi-key": "e138a5e326mshee193d9b02e3dafp19cc7djsn4aa0ac2244fa"
+    //   }
+    // }
+
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
 
 
   };
